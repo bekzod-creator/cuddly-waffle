e7e88590c147ec27da763899eec2267c934abced
DevOps <64070963+bekzod-creator@users.noreply.github.com>
2025-01-07 22:24:21 +0500

    Create version.js

---
+++ version.js
@@ -0,0 +1,12 @@
+{
+  "tverse": {
+    "version": "0.7.43",
+    "main_js": "app.js?0.7.43",
+    "js_hash": " 96f3d0911d35111ecda067357ab4b45a5070e2850f846d6333a7c8163a9fb02e"
+  },
+  "zoo": {
+    "version": "0.0.0",
+    "main_js": "index-DHGg822q.js",
+    "js_hash": "07a62d79df0b68862ffbf1f9c9be592feb7f9917a63163f511b73bd6683d9c58"
+  }
+}
